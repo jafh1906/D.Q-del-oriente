6ed76e0847e1422a39dadc90405ea4e39c1effb9
jafh1906 <juanfrancoclasesvirtuales@gmail.com>
2025-03-28 12:00:57 -0500

    Contenido del Layout

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import { LayoutApp } from "../Layouts/LayoutApp";
+
+export const Home = () => {
+  return (
+    <LayoutApp>
+      <div>
+        <h3>Soy Timido</h3>
+      </div>
+    </LayoutApp>
+  );
+};
