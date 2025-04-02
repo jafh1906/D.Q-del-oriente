5058ad55df2bbba339f5e3ed86f179192633778f
jafh1906 <juanfrancoclasesvirtuales@gmail.com>
2025-04-02 02:42:15 -0500

    se realizaron cambios

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,10 +5,10 @@ import { FaLocationDot } from "react-icons/fa6";
 
 export const Footer = () => {
   return (
-    <footer className="bg-[#006b96] text-white">
+    <footer className="letra-2 bg-[#006b96] text-white">
       <div className="flex w-full justify-between px-[10%] pt-[3%] pb-[2%]">
 
-        <div className="w-[200px] mt-[-25px]">
+        <div className="w-[210px] mt-[-25px]">
           <img src={LOGO} className='' alt="LOGO" />
           <p className='pl-4.5'>
           en este texto podemos escribir un lema o dicho corto que describa la empresa con el objetivo de dar un mensaje comercial sobre nosotros 
@@ -16,7 +16,7 @@ export const Footer = () => {
         </div>
 
         <div className="flex w-auto flex-col gap-4">
-          <h2 className="letra text-xl">Acerca de</h2>
+          <h2 className="letra-1 text-xl">Acerca de</h2>
           <div className="flex flex-col gap-3">
             <li>Servicios</li>
             <li>Productos</li>
@@ -25,7 +25,7 @@ export const Footer = () => {
         </div>
 
         <div className="flex w-auto flex-col gap-4">
-          <h2 className="letra text-xl">Contáctanos</h2>
+          <h2 className="letra-1 text-xl">Contáctanos</h2>
           <div className="flex flex-col gap-3">
             <p className='flex items-center gap-2'>
               <FaPhone />
@@ -39,7 +39,7 @@ export const Footer = () => {
         </div>
 
         <div className="flex w-aunto flex-col gap-4">
-          <h2 className="letra text-xl">Encuentranos</h2>
+          <h2 className="letra-1 text-xl">Encuentranos</h2>
           <div className="flex flex-col gap-3">
             <p className='flex items-center gap-2'>
               <FaLocationDot />
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,9 +1,22 @@
+// import bgNavBar from '../assets/images/bgNavBar.png'
+import bgNavBar2 from '../assets/images/bgNavBar2.png'
+import LOGO from '../assets/images/LOGO.webp'
 
 export const NavBar = () => {
   return (
     <>
-    <div>
-      NavBar
+    <div className="h-[150px] mt-[-4px] letra-2 bg-cover bg-center px-[10%]" style={{ backgroundImage: `url(${bgNavBar2})` }}>
+      <div className='flex items-center justify-between'>
+        <div className='w-[200px]'>
+          <img src={LOGO} alt="logo" />
+        </div>
+        <div className='flex gap-10 text-white'>
+          <div>Inicio</div>
+          <div>Productos</div>
+          <div>Servicios</div>
+          <div>Nosotros</div>
+        </div>
+      </div>
     </div>
     </>
   )
