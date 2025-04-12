b527e40c7232e29336c9bfc9edf1044edba012f9
jafh1906 <juanfrancoclasesvirtuales@gmail.com>
2025-04-12 15:50:44 -0500

    Hero renovado

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,25 +1,40 @@
+import HeroImage from "../assets/images/HeroImage.webp";
+
 export const Hero = () => {
   return (
-    <section className="flex items-center max-w-[1440px] mx-auto px-10 mt-10">
-      <div className="flex justify-between gap-10">
-        <div className="flex flex-col gap-3">
-          <h1 className="text-4xl">
-            Want anything to be <br /> easy with{" "}
-            <span className="font-bold">LaslesVPN.</span>
-          </h1>
-          <p className="text-gray-400">
-            Provide a network for all your needs with ease and fun using
-            LaslesVPN discover interesting features from us.
-          </p>
-          <div className="flex gap-5 mt-3">
-            <button className="bg-[#00AEEE] py-2 px-10 rounded-lg text-white font-bold shadow-[#00AEEE]/40 shadow-lg">
-              Productos
-            </button>
-            <button>Saber mas</button>
-          </div>
+    <section className="container mx-auto flex flex-col md:flex-row justify-between items-center pt-30 pb-6 px-4 sm:px-6 lg:px-8">
+      <div className="w-full md:w-1/2 space-y-8">
+        <div className="flex items-center gap-2 bg-gray-50 w-fit px-4 py-2 rounded-full hover:bg-gray-100 transition-colors cursor-pointer group">
+          <span className="text-cyan-500 group-hover:text-amber-300 group-hover:scale-110 transition-transform">
+            ★
+          </span>
+          <span className="text-sm font-medium">Soy timidin el timido</span>
         </div>
-        <div className="flex flex-center h-[382px] w-[611px]">
-          <img src="" alt="" />
+
+        <h1 className="text-4xl md:text-4xl lg:text-5xl font-bold leading-tight">
+          Soy timido el timido
+          <span className="text-cyan-500 relative inline-block">
+            Timidin el timido hola
+            <span className="absolute bottom-0 left-0 w-full h-0.5 bg-blue-200/60"></span>
+          </span>
+          <br />
+          Timidos
+          <span className="inline-block ml-2 animate-pulse">⏰</span>
+        </h1>
+
+        <p className="text-gray-600 text-lg md:text-xl max-w-xl">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
+          repellendus eveniet magnam
+        </p>
+      </div>
+
+      <div className="w-full md:w-1/2 mt-16 md:mt-0 pl-0 md:pl-12">
+        <div className="relative">
+          <img
+            src={HeroImage}
+            alt=""
+            className="rounded-lg relative z-10 hover:scale-[1.02] transition-transform duration-300"
+          />
         </div>
       </div>
     </section>
