09465c5f2ef3caedcb59362d064290459fd9f5d1
ArnosBC <bermudezjhon2006@gmail.com>
2025-08-25 11:29:21 -0500

    Boton de WhatsApp agregado

--- src/Components/Wsp.jsx
+++ src/Components/Wsp.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { FaWhatsapp } from "react-icons/fa";
 
 export const Wsp = () => {
   return (
-    <section>
-        <div>
-            
-        </div>
-    </section>
-  )
-}
+    <div>
+      <a
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-6 right-6 bg-green-500 p-4 rounded-full shadow-lg hover:bg-green-600 transition z-50"
+      >
+        <FaWhatsapp className="text-white text-3xl" />
+      </a>
+    </div>
+  );
+};
